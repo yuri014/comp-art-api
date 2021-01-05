cb89f2d712a06178eb49fcc34b71dc61fcb6b641
yuri014 <yuriflores2012@gmail.com>
2021-01-05 16:57:40 -0300

    adding routes and express.json

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import 'reflect-metadata';
-import cors from 'cors';
 import debug from 'debug';
 
 import './database/connection';
+import routes from './routes';
 
 const app = express();
 const PORT = 3333;
 
-app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 app.listen(PORT, () => debug.log(`Server running at: ${PORT}`));
