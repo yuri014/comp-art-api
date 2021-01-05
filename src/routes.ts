27b2f29af7937bea64583cc1126789d4ce6b5ac0
yuri014 <yuriflores2012@gmail.com>
2021-01-05 20:08:20 -0300

    adding authentication routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import Factory from './factory';
+
+import UserFactory from './factory/userFactory';
+import authMiddleware from './middlewares/Auth';
 
 const routes = Router();
 
-const user = Factory.generateUser();
+const { user, authUser } = UserFactory.generateInstance();
 
 routes.post('/users', user.store);
+routes.post('/auth', authUser.authenticate);
+
+routes.get('/users', authMiddleware, user.index);
 
 export default routes;
