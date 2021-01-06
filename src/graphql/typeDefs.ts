f6d3c0ecd88b2d2bd8feedc922cd87147502e630
yuri014 <yuriflores2012@gmail.com>
2021-01-06 19:44:26 -0300

    typeDef for user, register user and post

---
+++ src/graphql/typeDefs.ts
@@ -0,0 +1,35 @@
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    username: String!
+    email: String!
+    password: String!
+    token: String!
+    createdAt: String!
+  }
+
+  type Post {
+    id: ID!
+    description: String!
+    username: String!
+    name: String!
+  }
+
+  input RegisterInput {
+    username: String!
+    email: String!
+    password: String!
+    confirmPassoword: String!
+  }
+
+  type Query {
+    getPosts: [Post]
+  }
+
+  type Mutation {
+    register(registerInput: RegisterInput): User!
+  }
+`;
+export default typeDefs;
