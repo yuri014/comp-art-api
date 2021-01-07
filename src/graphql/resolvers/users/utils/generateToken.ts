dab2cf33f0fdc0312609a18df629307743dd663d
yuri014 <yuriflores2012@gmail.com>
2021-01-07 20:15:10 -0300

    generate token for recover password

--- src/graphql/resolvers/users/utils/generateToken.ts
+++ src/graphql/resolvers/users/utils/generateToken.ts
@@ -2,6 +2,9 @@ import jwt from 'jsonwebtoken';
 
 import { IUser } from '../../../../interfaces/User';
 
+export const generateRecoverPasswordToken = (email: string) =>
+  jwt.sign({ email }, process.env.SECRET as string, { expiresIn: '10m' });
+
 const generateToken = (result: IUser) =>
   jwt.sign(
     { id: result.id, email: result.email, username: result.username },
