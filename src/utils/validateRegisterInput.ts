d2b2cd9afad8adb3c861845a918bbe4b027f4014
yuri014 <yuriflores2012@gmail.com>
2021-01-07 00:04:19 -0300

    validation with joi

--- src/utils/validateRegisterInput.ts
+++ src/utils/validateRegisterInput.ts
@@ -1,4 +1,5 @@
 import { IRegisterFields } from '../interfaces/User';
+import userValidateSchema from '../validators/userSchema';
 
 const isNotEmpty = (field: string) => field.trim() === '';
 
@@ -19,27 +20,13 @@ export const validateLoginInput = (username: string, password: string) => {
   };
 };
 
-export const validateRegisterInput = (user: IRegisterFields) => {
-  const errors: { [key: string]: string } = {};
+export const validateRegisterInput = async (user: IRegisterFields) => {
+  const errors = userValidateSchema.validate({
+    username: user.username,
+    email: user.email,
+    password: user.password,
+    confirmPassword: user.confirmPassword,
+  });
 
-  if (isNotEmpty(user.email)) {
-    errors.email = 'Email não pode ser vazio';
-  } else if (!user.email.match(/^\S+@\S+$/)) {
-    errors.email = 'Email inválido';
-  }
-
-  if (isNotEmpty(user.password)) {
-    errors.password = 'Senha não pode ser vazia';
-  } else if (user.password.trim() !== user.confirmPassword.trim()) {
-    errors.password = 'Senhas não conferem';
-  }
-
-  if (isNotEmpty(user.username)) {
-    errors.username = 'Nome de usuário não pode ser vazio';
-  }
-
-  return {
-    errors,
-    valid: Object.keys(errors).length === 0,
-  };
+  return errors;
 };
