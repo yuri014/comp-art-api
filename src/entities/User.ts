7867f5521d0f83790a36377c33f0b89e8add660c
yuri014 <yuriflores2012@gmail.com>
2021-01-07 20:13:49 -0300

    adding default to confirmed email

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -6,7 +6,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  confirmed: Boolean,
+  confirmed: { type: Boolean, default: false },
   createdAt: String,
   updatedAt: String,
 });
