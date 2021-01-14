9a530a91dfd749039c068b336ecb7b29560c5dd1
yuri014 <yuriflores2012@gmail.com>
2021-01-13 22:43:35 -0300

    use graphqlUploadExpress

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,23 @@
-import express from 'express';
 import debug from 'debug';
-import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
 import mongoose from 'mongoose';
+import { ApolloServer } from 'apollo-server-express';
+import { graphqlUploadExpress } from 'graphql-upload';
 
 import typeDefs from './graphql/typeDefs';
 import resolvers from './graphql/resolvers';
 
 require('dotenv').config();
 
-const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => ({ req }) });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req }),
+  uploads: false,
+});
 
 const app = express();
-
+app.use(graphqlUploadExpress());
 server.applyMiddleware({ app, cors: { origin: process.env.FRONT_END_HOST } });
 app.use(express.urlencoded({ extended: true }));
 const PORT = 3333;
