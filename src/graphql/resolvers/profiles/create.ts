c69bad54271e06aae5823be273d3242d1d66c439
yuri014 <yuriflores2012@gmail.com>
2021-01-14 20:25:10 -0300

    create profile

---
+++ src/graphql/resolvers/profiles/create.ts
@@ -0,0 +1,40 @@
+import { UserInputError } from 'apollo-server-express';
+import { Model } from 'mongoose';
+
+import { IArtistProfile, ICreateProfile, IUserProfile } from '../../../interfaces/Profile';
+import { IToken } from '../../../interfaces/Token';
+import uploadImage from '../../../utils/uploadImage';
+
+const createProfile = async (
+  user: IToken,
+  Profile: Model<IArtistProfile> | Model<IUserProfile>,
+  data: ICreateProfile,
+) => {
+  const profileExists = await Profile.findOne({ owner: user.id });
+
+  const { avatar, bio, coverImage, name } = data;
+
+  if (profileExists) {
+    throw new UserInputError('Usuário já é dono de um perfil', {
+      errors: 'Usuário já é dono de um perfil',
+    });
+  }
+
+  const { file: avatarFile } = await avatar;
+  const avatarImageUrl = await uploadImage(avatarFile?.createReadStream, avatarFile?.filename);
+  const { file: coverFile } = await coverImage;
+  const coverImageUrl = await uploadImage(coverFile?.createReadStream, coverFile?.filename);
+
+  const newProfile = new Profile({
+    name,
+    avatar: avatarImageUrl,
+    bio,
+    coverImage: coverImageUrl,
+    createdAt: new Date().toISOString(),
+    owner: user.id,
+  });
+
+  await newProfile.save();
+};
+
+export default createProfile;
