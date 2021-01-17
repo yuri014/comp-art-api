3e085df99b7db435089d875422e2fc770805c556
yuri014 <yuriflores2012@gmail.com>
2021-01-17 01:16:15 -0300

    adding hashtags and links to schemas

--- src/entities/UserProfile.ts
+++ src/entities/UserProfile.ts
@@ -14,8 +14,19 @@ const UserProfileSchema = new Schema({
   createdAt: String,
   updatedAt: String,
   owner: {
-    type: Schema.Types.ObjectId,
+    type: Schema.Types.String,
     ref: 'users',
+  },
+  hashtags: { type: [String], maxlength: 6 },
+  links: {
+    soundcloud: String,
+    twitter: String,
+    facebook: String,
+    wattpad: String,
+    pinterest: String,
+    bandcamp: String,
+    deviantart: String,
+    customLink: String,
   },
 });
 
