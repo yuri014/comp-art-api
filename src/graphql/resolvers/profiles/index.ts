30247f03818e7a1d628fa712c290cd7edf190ded
yuri014 <yuriflores2012@gmail.com>
2021-01-21 02:14:48 -0300

    temporary follow mutation

--- src/graphql/resolvers/profiles/index.ts
+++ src/graphql/resolvers/profiles/index.ts
@@ -9,6 +9,8 @@ import UserProfile from '../../../entities/UserProfile';
 import profileValidationSchema from '../../../validators/profileSchema';
 import User from '../../../entities/User';
 import findProfile from './services/find';
+import Follower from '../../../entities/Follower';
+import Following from '../../../entities/Following';
 
 const profileResolvers: IResolvers = {
   Query: {
@@ -21,7 +23,7 @@ const profileResolvers: IResolvers = {
 
       const profile = await findProfile(user);
 
-      return profile;
+      return { ...profile._doc, isArtist: profile.isArtist };
     },
 
     async getLoggedProfile(parent, args, context) {
@@ -29,7 +31,7 @@ const profileResolvers: IResolvers = {
 
       const profile = await findProfile(user);
 
-      return profile;
+      return { ...profile._doc, isArtist: profile.isArtist };
     },
   },
   Mutation: {
@@ -64,6 +66,106 @@ const profileResolvers: IResolvers = {
       }
 
       await createProfile(user, UserProfile, createProfileInput);
+
+      return true;
+    },
+    async follow(_, { username }: { username: string }, context) {
+      const user = checkAuth(context);
+
+      const followedUser = await User.findOne({ username });
+
+      if (!followedUser) {
+        throw new UserInputError('Usuário não encontrado', {
+          errors: 'Usuário não encontrado',
+        });
+      }
+
+      const followed = await findProfile(followedUser);
+
+      const authProfile = await findProfile(user);
+
+      if (!authProfile) {
+        throw new Error();
+      }
+
+      const options = {
+        upsert: true,
+        new: true,
+        setDefaultsOnInsert: true,
+        useFindAndModify: false,
+      };
+
+      if (followed.isArtist && authProfile._doc) {
+        const profile = authProfile._doc;
+        await Follower.findOneAndUpdate(
+          {
+            username: followedUser.username,
+          },
+          {
+            $set: {
+              artistFollowers: {
+                avatar: profile.avatar,
+                owner: profile.owner,
+              },
+            },
+          },
+          options,
+        );
+      } else {
+        const profile = authProfile._doc;
+        if (profile) {
+          await Follower.findOneAndUpdate(
+            {
+              username: followedUser.username,
+            },
+            {
+              $set: {
+                userFollowers: {
+                  avatar: profile.avatar,
+                  owner: profile.owner,
+                },
+              },
+            },
+            options,
+          );
+        }
+      }
+
+      if (user.isArtist && followed._doc) {
+        const profile = followed._doc;
+        await Following.findOneAndUpdate(
+          {
+            username: user.username,
+          },
+          {
+            $set: {
+              artistFollowing: {
+                avatar: profile.avatar,
+                owner: profile.owner,
+              },
+            },
+          },
+          options,
+        );
+      } else {
+        const profile = followed._doc;
+        if (profile) {
+          await Following.findOneAndUpdate(
+            {
+              username: user.username,
+            },
+            {
+              $set: {
+                userFollowing: {
+                  avatar: followed.avatar,
+                  owner: followed.owner,
+                },
+              },
+            },
+            options,
+          );
+        }
+      }
 
       return true;
     },
