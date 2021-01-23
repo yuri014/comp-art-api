330b90f40938cba7226af27e3adc362d2cb853d1
yuri014 <yuriflores2012@gmail.com>
2021-01-23 18:44:39 -0300

    create post mutation

---
+++ src/graphql/resolvers/post/index.ts
@@ -0,0 +1,40 @@
+import { AuthenticationError } from 'apollo-server-express';
+import { IResolvers } from 'graphql-tools';
+import Post from '../../../entities/Post';
+
+import { IPostInput } from '../../../interfaces/Post';
+import checkAbilityToPost from '../../../middlewares/checkAbilityToPost';
+import checkAuth from '../../../middlewares/checkAuth';
+import uploadImage from '../../../utils/uploadImage';
+
+const postResolvers: IResolvers = {
+  Mutation: {
+    async createPost(_, { postInput }: { postInput: IPostInput }, context) {
+      const user = checkAuth(context);
+
+      if (!user.isArtist) {
+        throw new AuthenticationError('Somente artistas podem postar');
+      }
+
+      const profile = await checkAbilityToPost(user.username);
+      const post = { description: postInput.description.trim(), body: postInput.body };
+
+      const { file } = await post.body;
+
+      const imageUrl = await uploadImage(file.createReadStream, file.filename);
+
+      const newProfile = new Post({
+        description: post.description,
+        body: imageUrl,
+        createdAt: new Date().toISOString(),
+        artist: profile.owner,
+      });
+
+      await profile.update({ isBlockedToPost: true, postsRemainingToUnblock: 3 });
+
+      await newProfile.save();
+    },
+  },
+};
+
+export default postResolvers;
