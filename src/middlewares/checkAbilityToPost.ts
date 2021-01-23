c2b9049a88def48426a48957d1edb7da91aa62f8
yuri014 <yuriflores2012@gmail.com>
2021-01-23 18:43:39 -0300

    middleware to check if artist is able to post

---
+++ src/middlewares/checkAbilityToPost.ts
@@ -0,0 +1,19 @@
+import { UserInputError } from 'apollo-server-express';
+
+import ArtistProfile from '../entities/ArtistProfile';
+
+const checkAbilityToPost = async (username: string) => {
+  const profile = await ArtistProfile.findOne({ owner: username });
+
+  if (!profile) {
+    throw new UserInputError('Perfil não encontrado!');
+  }
+
+  if (profile.isBlockedToPost) {
+    throw new UserInputError('Perfil bloqueado para postar');
+  }
+
+  return profile;
+};
+
+export default checkAbilityToPost;
