56fbef354c769ec8ae14e0d42b4dd90e1dcc7c34
yuri014 <yuriflores2012@gmail.com>
2021-01-25 21:13:28 -0300

    fix to create a user input error for file size

--- src/utils/uploadImage.ts
+++ src/utils/uploadImage.ts
@@ -1,15 +1,21 @@
 import path from 'path';
 import fs, { ReadStream } from 'fs-extra';
+import { UserInputError } from 'apollo-server-express';
 
 const uploadImage = async (createReadStream?: () => ReadStream, filename?: string) => {
-  if (createReadStream && filename) {
-    const stream = createReadStream();
-    const originalName = `${Date.now()}-${filename}`;
-    const pathName = path.join(__dirname, '..', '..', `/public/uploads/images/${originalName}`);
+  try {
+    if (createReadStream && filename) {
+      const stream = createReadStream();
+      const originalName = `${Date.now()}-${filename}`;
+      const pathName = path.join(__dirname, '..', '..', `/public/uploads/images/${originalName}`);
 
-    await stream.pipe(fs.createWriteStream(pathName));
-    return `${process.env.HOST}/uploads/images/${originalName}`;
+      stream.pipe(fs.createWriteStream(pathName));
+      return `${process.env.HOST}/uploads/images/${originalName}`;
+    }
+  } catch (error) {
+    throw new UserInputError('Limite máximo de 1MB.');
   }
+
   return '';
 };
 
