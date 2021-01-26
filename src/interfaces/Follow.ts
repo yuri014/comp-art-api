7c0b4aa20f7c107a5a02b8a40e9f88ff1e5b79aa
yuri014 <yuriflores2012@gmail.com>
2021-01-26 00:02:48 -0300

    fix follow profile type

--- src/interfaces/Follow.ts
+++ src/interfaces/Follow.ts
@@ -1,9 +1,11 @@
 import { Document } from 'mongoose';
 
-type FollowProfile = {
-  avatar: string;
-  owner: string;
-};
+type FollowProfile = [
+  {
+    avatar: string;
+    owner: string;
+  },
+];
 
 export interface IFollower extends Document {
   _doc: IFollower;
