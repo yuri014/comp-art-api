1e62ceb72a3059bb2e3b30b15d44b22a3e915a83
yuri014 <yuriflores2012@gmail.com>
2021-01-31 01:27:53 -0300

    update post middleware validation for postsRemainingToUnblock

--- src/middlewares/checkAbilityToPost.ts
+++ src/middlewares/checkAbilityToPost.ts
@@ -13,7 +13,7 @@ const checkAbilityToPost = async (username: string) => {
     throw new UserInputError('Perfil bloqueado para postar');
   }
 
-  if (profile.postsRemainingToUnblock === 0) {
+  if (profile.postsRemainingToUnblock > 0) {
     throw new UserInputError('Perfil bloqueado para postar');
   }
 
