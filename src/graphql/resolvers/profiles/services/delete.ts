07c5e3ad90e102b2454439929112961ec42a4247
yuri014 <yuriflores2012@gmail.com>
2021-01-31 20:13:35 -0300

    delete follows validations

--- src/graphql/resolvers/profiles/services/delete.ts
+++ src/graphql/resolvers/profiles/services/delete.ts
@@ -1,8 +1,10 @@
+import { UserInputError } from 'apollo-server-express';
 import ArtistProfile from '../../../../entities/ArtistProfile';
 import Follower from '../../../../entities/Follower';
 import Following from '../../../../entities/Following';
 import UserProfile from '../../../../entities/UserProfile';
-import { IArtistProfile, IUserProfile } from '../../../../interfaces/Profile';
+import { IProfileView } from '../../../../interfaces/Profile';
+import { isAlreadyFollow, isAlreadyFollowing } from '../../../../middlewares/isAlreadyFollow';
 
 const options = {
   new: true,
@@ -11,10 +13,19 @@ const options = {
 
 export const unfollower = async (
   isArtist: boolean,
-  profileThatFollows: IUserProfile | IArtistProfile,
+  profileThatFollows: IProfileView,
   userWhoIsFollowed: string,
 ) => {
+  const { artistFollower, userFollower } = await isAlreadyFollow(
+    profileThatFollows.owner,
+    userWhoIsFollowed,
+  );
+
   if (isArtist) {
+    if (!artistFollower) {
+      throw new UserInputError('Não é seguido');
+    }
+
     await ArtistProfile.findOneAndUpdate(
       { owner: profileThatFollows.owner },
       { $inc: { following: -1 } },
@@ -35,6 +46,10 @@ export const unfollower = async (
       },
       options,
     );
+  }
+
+  if (!userFollower) {
+    throw new UserInputError('Não é seguido');
   }
 
   await UserProfile.findOneAndUpdate(
@@ -61,10 +76,19 @@ export const unfollower = async (
 
 export const unfollowing = async (
   isArtist: boolean,
-  profileThatIsFollowing: IUserProfile | IArtistProfile,
+  profileThatIsFollowing: IProfileView,
   userWhoIsFollowing: string,
 ) => {
+  const { artistFollowing, userFollowing } = await isAlreadyFollowing(
+    profileThatIsFollowing.owner,
+    userWhoIsFollowing,
+  );
+
   if (isArtist) {
+    if (!artistFollowing) {
+      throw new UserInputError('Já é seguido');
+    }
+
     await ArtistProfile.findOneAndUpdate(
       { owner: profileThatIsFollowing.owner },
       { $inc: { followers: -1 } },
@@ -87,6 +111,10 @@ export const unfollowing = async (
       },
       options,
     );
+  }
+
+  if (!userFollowing) {
+    throw new UserInputError('Não é seguido');
   }
 
   await UserProfile.findOneAndUpdate(
