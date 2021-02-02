3916a74f1c01f3668fe9a50e27a72ccc35c1dd9f
yuri014 <yuriflores2012@gmail.com>
2021-02-02 18:03:41 -0300

    query resolver to return a boolean for isFollowing

--- src/graphql/resolvers/profiles/index.ts
+++ src/graphql/resolvers/profiles/index.ts
@@ -11,6 +11,7 @@ import User from '../../../entities/User';
 import findProfile from './services/find';
 import { follower, following } from './services/update';
 import { unfollower, unfollowing } from './services/delete';
+import { isAlreadyFollow } from '../../../middlewares/isAlreadyFollow';
 
 const profileResolvers: IResolvers = {
   Query: {
@@ -32,6 +33,19 @@ const profileResolvers: IResolvers = {
       const profile = await findProfile(user);
 
       return { ...profile._doc, isArtist: profile.isArtist };
+    },
+
+    async getIsFollowing(_, { username }: { username: string }, context) {
+      const user = checkAuth(context);
+
+      const { artistFollower, userFollower } = await isAlreadyFollow(user.username, username);
+
+      if (!artistFollower || !userFollower) return false;
+      if (artistFollower.artistFollowers.length > 0 || userFollower.userFollowers.length > 0) {
+        return true;
+      }
+
+      return false;
     },
   },
   Mutation: {
