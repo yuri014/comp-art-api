f4f7f7f11fd9f36909453fe9b7fb48dceece2d97
yuri014 <yuriflores2012@gmail.com>
2021-02-05 23:35:38 -0300

    upload audio and image

---
+++ src/utils/upload.ts
@@ -0,0 +1,52 @@
+import path from 'path';
+import fs, { ReadStream } from 'fs-extra';
+import { UserInputError } from 'apollo-server-express';
+
+export const uploadImage = async (createReadStream?: () => ReadStream, filename?: string) => {
+  if (filename && !filename.match(/\.(png|jpg|jpeg|webp)$/)) {
+    throw new UserInputError('Upload precisa ser em um formato de imagem suportado.', {
+      errors: 'Formatos de imagens suportados: png, webp, jpg e jpeg.',
+    });
+  }
+
+  try {
+    if (createReadStream && filename) {
+      const stream = createReadStream();
+      const originalName = `${Date.now()}-${filename}`;
+      const pathName = path.join(__dirname, '..', '..', `/public/uploads/images/${originalName}`);
+
+      stream.pipe(fs.createWriteStream(pathName));
+      return `${process.env.HOST}/uploads/images/${originalName}`;
+    }
+  } catch (error) {
+    throw new UserInputError('Limite máximo excedido.', {
+      errors: 'Limite máximo de upload: 3MB.',
+    });
+  }
+
+  return '';
+};
+
+export const uploadAudio = async (createReadStream?: () => ReadStream, filename?: string) => {
+  if (filename && !filename.match(/\.(mp3|wav)$/)) {
+    throw new UserInputError('Upload precisa ser em um formato de áudio suportado.', {
+      errors: 'Formatos de imagens suportados: mp3 e wav.',
+    });
+  }
+
+  try {
+    if (createReadStream && filename) {
+      const stream = createReadStream();
+      const originalName = `${Date.now()}-${filename}`;
+      const pathName = path.join(__dirname, '..', '..', `/public/uploads/audio/${originalName}`);
+
+      stream.pipe(fs.createWriteStream(pathName));
+      return `${process.env.HOST}/uploads/audio/${originalName}`;
+    }
+  } catch (error) {
+    throw new UserInputError('Limite máximo excedido.', {
+      errors: 'Limite máximo de upload: 3MB.',
+    });
+  }
+  return '';
+};
