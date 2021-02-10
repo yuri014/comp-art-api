931cfa0a8bae863cbac2d16d77ab0c75d2d9f636
yuri014 <yuriflores2012@gmail.com>
2021-02-09 23:53:03 -0300

    deletePost service

--- src/graphql/resolvers/post/services/delete.ts
+++ src/graphql/resolvers/post/services/delete.ts
@@ -1,9 +1,9 @@
-import { UserInputError } from 'apollo-server-express';
+import { AuthenticationError, UserInputError } from 'apollo-server-express';
 
 import Post from '../../../../entities/Post';
 import { IToken } from '../../../../interfaces/Token';
 
-const dislikePost = async (id: string, user: IToken) => {
+export const dislikePost = async (id: string, user: IToken) => {
   const post = await Post.findById(id);
 
   if (!post) {
@@ -39,4 +39,22 @@ const dislikePost = async (id: string, user: IToken) => {
   return true;
 };
 
-export default dislikePost;
+export const deletePostService = async (id: string, user: IToken) => {
+  const post = await Post.findById(id);
+
+  if (!post) {
+    throw new UserInputError('Não há post');
+  }
+
+  if (post.artist.username !== user.username) {
+    throw new AuthenticationError('Você não é o autor deste post');
+  }
+
+  try {
+    await post.deleteOne();
+  } catch (error) {
+    throw new Error(error);
+  }
+
+  return true;
+};
