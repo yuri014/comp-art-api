a4ae3852706da11301c8af30d629c81a2caeeef3
yuri014 <yuriflores2012@gmail.com>
2021-02-10 23:36:30 -0300

    getUser token util

---
+++ src/utils/getUser.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+import { IToken } from '../interfaces/Token';
+
+const getUser = (token: string) => {
+  if (token) {
+    return jwt.verify(token, process.env.SECRET as string) as IToken;
+  }
+  return {
+    username: '',
+  };
+};
+
+export default getUser;
