4af33e24ee00101de27b6f73210bc3814c44687b
yuri014 <yuriflores2012@gmail.com>
2021-02-16 01:47:31 -0300

    update comment schema

--- src/entities/Comments.ts
+++ src/entities/Comments.ts
@@ -3,12 +3,12 @@ import mongoose, { Schema } from 'mongoose';
 import { Comment } from '../interfaces/Comment';
 
 const CommentSchema = new Schema({
-  on: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+  post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
   comments: [
     {
       author: { type: Schema.Types.ObjectId, required: true, refPath: 'onModel' },
       onModel: { type: String, required: true, enum: ['ArtistProfile', 'UserProfile'] },
-      body: String,
+      body: { type: String, required: true },
       createdAt: String,
     },
   ],
