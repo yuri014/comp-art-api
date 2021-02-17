0b2703d69302dd9a497c71b3dc500cfb95ea796d
yuri014 <yuriflores2012@gmail.com>
2021-02-17 02:05:28 -0300

    fix ref path

--- src/entities/Comments.ts
+++ src/entities/Comments.ts
@@ -6,7 +6,7 @@ const CommentSchema = new Schema({
   post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
   comments: [
     {
-      author: { type: Schema.Types.ObjectId, required: true, refPath: 'onModel' },
+      author: { type: Schema.Types.ObjectId, required: true, refPath: 'comments.onModel' },
       onModel: { type: String, required: true, enum: ['ArtistProfile', 'UserProfile'] },
       body: { type: String, required: true },
       createdAt: String,
