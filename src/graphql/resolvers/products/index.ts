ec034aa0ee6899d14c2aaa407d045359973fcbdd
yuri014 <yuriflores2012@gmail.com>
2021-03-13 22:31:24 -0300

    register product

--- src/graphql/resolvers/products/index.ts
+++ src/graphql/resolvers/products/index.ts
@@ -1,7 +1,10 @@
-import { AuthenticationError, IResolvers } from 'apollo-server-express';
+import { AuthenticationError, IResolvers, UserInputError } from 'apollo-server-express';
+
+import { uploadImage } from '../../../utils/upload';
 import { IProductInput } from '../../../interfaces/Product';
 import checkAuth from '../../../middlewares/checkAuth';
 import postValidationSchema from '../../../validators/postSchema';
+import Product from '../../../entities/Product';
 
 const productsResolvers: IResolvers = {
   Query: {},
@@ -13,7 +16,35 @@ const productsResolvers: IResolvers = {
         throw new AuthenticationError('Somente artistas podem criar produtos.');
       }
 
-      postValidationSchema.validate(productInput);
+      const errors = postValidationSchema.validate(productInput);
+
+      if (errors.error) {
+        throw new UserInputError('Erros', {
+          errors: errors.error.message,
+        });
+      }
+
+      const { category, images, name, phone, value, description } = productInput;
+
+      const uploadedImages = await images;
+
+      const imagesUrl = await Promise.all(
+        uploadedImages.map(({ file }) => uploadImage(file.createReadStream, file.filename)),
+      );
+
+      const newProduct = new Product({
+        name: name.trim(),
+        artist: user.username,
+        category: category.trim(),
+        phone: phone.trim(),
+        value,
+        description: description?.trim(),
+        images: imagesUrl,
+      });
+
+      await newProduct.save();
+
+      return true;
     },
   },
 };
