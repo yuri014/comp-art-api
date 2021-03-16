e4df4110fbff8c41a61a032fde7b3e79afe2e219
yuri014 <yuriflores2012@gmail.com>
2021-03-16 20:07:29 -0300

    adding profile validation util

---
+++ src/graphql/resolvers/profiles/services/utils/profileValidation.ts
@@ -0,0 +1,47 @@
+import { UserInputError } from 'apollo-server-express';
+import { IArtistProfile, IUserProfile } from '../../../../../interfaces/Profile';
+
+import { IToken } from '../../../../../interfaces/Token';
+import profileValidationSchema from '../../../../../validators/profileSchema';
+
+const canCreateProfile = async (
+  user: IToken,
+  name: string,
+  bio: string,
+  hashtags: Array<string>,
+) => {
+  if (!user) {
+    throw new UserInputError('Usuário não encontrado', {
+      errors: 'Usuário não encontrado',
+    });
+  }
+
+  const errors = profileValidationSchema.validate({
+    name,
+    bio,
+  });
+
+  if (errors.error) {
+    throw new UserInputError('Erros', {
+      errors: errors.error.message,
+    });
+  }
+
+  const hashtagsLength = hashtags.length;
+
+  if (hashtagsLength > 5 || hashtagsLength !== new Set(hashtags).size) {
+    throw new UserInputError('Limite de 5 hashtags não repetidas', {
+      errors: 'Limite de 5 hashtags não repetidas',
+    });
+  }
+
+  return async (profileExists: IArtistProfile | IUserProfile | null) => {
+    if (profileExists) {
+      throw new UserInputError('Usuário já é dono de um perfil', {
+        errors: 'Usuário já é dono de um perfil',
+      });
+    }
+  };
+};
+
+export default canCreateProfile;
