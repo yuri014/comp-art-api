41b7b1325c15ef4f65bf5eea84341db96d33b5db
yuri014 <yuriflores2012@gmail.com>
2021-03-19 22:50:29 -0300

    adding news field in product interface

--- src/interfaces/Product.ts
+++ src/interfaces/Product.ts
@@ -1,7 +1,8 @@
 import { Document } from 'mongoose';
 
-import { IArtistProfile } from './Profile';
 import { IUpload } from './Upload';
+import { IOnModel } from './General';
+import { IArtistProfile } from './Profile';
 
 interface Product {
   name: string;
@@ -15,6 +16,14 @@ export interface IProduct extends Document, Product {
   _doc?: IProduct;
   image: Array<string>;
   createdAt: string;
+  reviewsStars: Array<number>;
+  reviews: Array<{
+    author: string;
+    onModel: IOnModel;
+    review: string;
+    createdAt: string;
+  }>;
+  totalSales: { type: Number; required: true; default: 0 };
   artist: string | IArtistProfile;
 }
 
