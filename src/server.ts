496450c8a50d5215287ddf684a3244de3ae1dca1
yuri014 <yuriflores2012@gmail.com>
2021-04-25 19:18:01 -0300

    removing app from server file

--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,13 @@
 import debug from 'debug';
-import express, { Request, Response } from 'express';
+import https from 'https';
+import fs from 'fs-extra';
 import mongoose from 'mongoose';
-import { ApolloServer } from 'apollo-server-express';
-import { graphqlUploadExpress } from 'graphql-upload';
 
-import typeDefs from './graphql/definitions';
-import resolvers from './graphql/resolvers';
-import rateLimiterMiddleware from './middlewares/limiter';
-
-const https = require('https');
-const fs = require('fs');
+import app, { PORT } from './app';
 
 require('dotenv').config({
   path: process.env.NODE_ENV === 'development' ? '.env.development' : '.env',
 });
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => ({ req }),
-  uploads: false,
-});
-
-const app = express();
-app.use(rateLimiterMiddleware);
-
-app.use(express.static('public'));
-
-app.get('/', (req: Request, res: Response) => {
-  res.send({
-    title: 'CompArt API',
-    version: '0.0.1',
-    sponsor: 'https://www.catarse.me/compart_6d8c?ref=project_link',
-    website: 'https://comp-art.vercel.app/',
-  });
-});
-
-app.use(
-  graphqlUploadExpress({
-    maxFileSize: 8000000,
-    maxFiles: 2,
-  }),
-);
-server.applyMiddleware({ app, cors: { origin: process.env.FRONT_END_HOST } });
-app.use(express.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 3333;
 
 const options = {
   // tls
