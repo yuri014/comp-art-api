a334e18b3ef2aed0c99c5f6522bb7ce2798bb883
yuri014 <yuriflores2012@gmail.com>
2021-04-28 20:22:37 -0300

    organizing queries and mutations

--- src/graphql/definitions/mutation.ts
+++ src/graphql/definitions/mutation.ts
@@ -2,24 +2,37 @@ import { gql } from 'apollo-server-express';
 
 const mutations = gql`
   type Mutation {
+    #users
     register(registerInput: RegisterInput!): Boolean
     login(email: String!, password: String!): User!
     confirmationEmail(code: String!, email: String!): User!
     sendForgotPasswordEmail(email: String!): Boolean
     recoverPassword(token: String!, newPassword: String!): String!
+
+    #profiles
     createProfile(createProfileInput: CreateProfileInput!): Boolean
+    updateProfile(newProfileInput: CreateProfileInput): Boolean
+
+    #follows
     follow(username: String!): Boolean
     unfollow(username: String!): Boolean
+
+    #posts
     createPost(postInput: CreatePostInput!): Boolean
     deletePost(id: ID!): Boolean
+
+    #post interactions
     like(id: ID!): Boolean
     dislike(id: ID!): Boolean
     comment(postID: ID!, comment: String!): Boolean
     deleteComment(id: ID!): Boolean
-    updateProfile(newProfileInput: CreateProfileInput): Boolean
-    createSharePost(shareInput: SharePost!): Boolean
     likeComment(id: ID!): Boolean
     dislikeComment(id: ID!): Boolean
+
+    #share
+    createSharePost(shareInput: SharePost!): Boolean
+
+    #product
     createProduct(productInput: ProductInput!): Boolean
   }
 `;
--- src/graphql/definitions/query.ts
+++ src/graphql/definitions/query.ts
@@ -14,20 +14,27 @@ const queries = gql`
   union Timeline = Post | Share
 
   type Query {
-    getProfile(username: String!): Profile
+    #profiles
     getLoggedProfile: Profile!
+    getSuggestedProfiles: [Profile]
+    getProfile(username: String!): Profile
+    searchProfiles(query: String!, offset: Int!, limit: Int!): [Profile]
+
+    #follows
+    getIsFollowing(id: String!): Boolean
+    getFollowers(offset: Int!, username: String!): [Profile]
+    getFollowing(offset: Int!, username: String!): [Profile]
+
+    #posts
     getPost(id: ID!): Post
     getPosts(offset: Int!): [Timeline]
     getProfilePosts(offset: Int!, username: String!): [Post]
-    getIsFollowing(id: String!): Boolean
     getExplorePosts(offset: Int!): [Post]
+    searchPost(query: String!, offset: Int!): [Post]
+
+    #post interactions
     getComments(postID: ID!, offset: Int!): [Comments]
     getLikes(postID: ID!, offset: Int!): [Profile]
-    getFollowers(offset: Int!, username: String!): [Profile]
-    getFollowing(offset: Int!, username: String!): [Profile]
-    searchProfiles(query: String!, offset: Int!, limit: Int!): [Profile]
-    searchPost(query: String!, offset: Int!): [Post]
-    getSuggestedProfiles: [Profile]
   }
 `;
 
