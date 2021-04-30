ae3fd96bcc90dd99104519ef01e4ca6124fb3968
yuri014 <yuriflores2012@gmail.com>
2021-04-29 23:33:14 -0300

    using like content in share resolver

--- src/graphql/resolvers/share/index.ts
+++ src/graphql/resolvers/share/index.ts
@@ -1,7 +1,9 @@
 import { IResolvers } from 'apollo-server-express';
+import Share from '../../../entities/Share';
 
 import { IShareInput } from '../../../interfaces/Share';
 import checkAuth from '../../../middlewares/checkAuth';
+import likeContent from '../../../utils/likeContent';
 import createShare from './services/create';
 import deleteShareService from './services/delete';
 
@@ -16,6 +18,14 @@ const shareResolvers: IResolvers = {
       const user = checkAuth(context);
 
       return deleteShareService(id, user);
+    },
+
+    async likeShare(_, { id }: { id: string }, context) {
+      const user = checkAuth(context);
+
+      await likeContent(id, user, Share);
+
+      return true;
     },
   },
 };
