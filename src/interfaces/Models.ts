37831e5ff39d32fa013373390dbf57eede418e23
yuri014 <yuriflores2012@gmail.com>
2021-04-30 00:40:37 -0300

    adding profile entity

--- src/interfaces/Models.ts
+++ src/interfaces/Models.ts
@@ -1,6 +1,9 @@
 import { Model } from 'mongoose';
 
 import { IPost } from './Post';
+import { IArtistProfile, IUserProfile } from './Profile';
 import { IShare } from './Share';
 
 export type PostEntity = Model<IPost> | Model<IShare>;
+
+export type IProfileEntity = IArtistProfile | IUserProfile;
