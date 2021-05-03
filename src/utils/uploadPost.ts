1e1d2eb7aa68b54a60a7c358ee14a260a14abd4d
yuri014 <yuriflores2012@gmail.com>
2021-05-03 20:33:46 -0300

    adding util to post uploads

---
+++ src/utils/uploadPost.ts
@@ -0,0 +1,22 @@
+import { IUpload } from '../interfaces/Upload';
+import { uploadAudio, uploadImage } from './upload';
+
+export const uploadBody = async (fileBody: Promise<IUpload>, mediaId: number) => {
+  const { file } = await fileBody;
+  const audioId = 2;
+
+  if (mediaId === audioId) {
+    return uploadAudio(file.createReadStream, file.filename);
+  }
+
+  return uploadImage(file.createReadStream, file.filename);
+};
+
+export const uploadThumbnail = async (thumbnail: Promise<IUpload> | undefined) => {
+  if (thumbnail) {
+    const { file: thumbnailFile } = await thumbnail;
+    return uploadImage(thumbnailFile.createReadStream, thumbnailFile.filename);
+  }
+
+  return '';
+};
