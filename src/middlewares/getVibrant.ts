bbff33bb9ab0ac0c8fa15dac2451616c21e7dc75
Leonardo Flores <contatoleonardoflores@gmail.com>
2021-05-05 00:53:21 -0300

    add middleware getVibrant for pallet theme in frontend

---
+++ src/middlewares/getVibrant.ts
@@ -0,0 +1,29 @@
+import tinyColor from 'tinycolor2';
+import Vibrant from 'node-vibrant';
+
+/**
+ * Verifica acessibilidade da cor
+ * @returns cor Predominante ou cor secundária
+ */
+const getVibrant = async (path: string, themeColor: string) => {
+  const palette = await Vibrant.from(path).getPalette();
+
+  const color = palette.Vibrant?.hex;
+
+  if (!tinyColor(themeColor).isValid()) {
+    throw new Error(`Color ${themeColor} is invalid, please try again...`);
+  }
+
+  const hasColorAcessibility = tinyColor.isReadable(`${color}`, `${themeColor}`, {
+    level: 'AA',
+    size: 'large',
+  });
+
+  if (!hasColorAcessibility) {
+    return palette.LightVibrant?.hex;
+  }
+
+  return color;
+};
+
+export default getVibrant;
