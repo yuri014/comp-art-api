eca16df09dbd12952f80fad9ecd2eb7c74e1c88c
yuri014 <yuriflores2012@gmail.com>
2021-05-12 00:23:29 -0300

    alterando limite do texto

--- src/validators/postSchema.ts
+++ src/validators/postSchema.ts
@@ -3,9 +3,9 @@ import Joi from 'joi';
 const isValidText = Joi.string();
 
 const postValidationSchema = Joi.object({
-  description: isValidText.allow('').max(255).messages({
+  description: isValidText.allow('').max(1200).messages({
     'string.base': 'Descrição deve ser um texto',
-    'string.min': 'Descrição tem limite de 255 caracteres',
+    'string.min': 'Descrição tem limite de 1200 caracteres',
   }),
   alt: isValidText.allow('').max(60).messages({
     'string.base': 'Texto alternativo deve ser um texto',
