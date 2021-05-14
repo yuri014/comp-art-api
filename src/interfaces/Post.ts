694ca3b45b01f2f09215ad10d848da2f9d2ad290
yuri014 <yuriflores2012@gmail.com>
2021-05-13 21:01:20 -0300

    removing media id from input interface

--- src/interfaces/Post.ts
+++ src/interfaces/Post.ts
@@ -6,13 +6,13 @@ import { IArtistProfile, IUserProfile } from './Profile';
 
 interface Post {
   description: string;
-  mediaId: number;
   alt?: string;
 }
 
 export interface IPost extends Document, Post {
   _doc?: IPost;
   body: string;
+  mediaId: number;
   likes: Array<{
     profile: IArtistProfile | IUserProfile | string;
     onModel: IOnModel;
