3fa9b0c14700440ef7684f928c83ffa2122c5cc7
yuri014 <yuriflores2012@gmail.com>
2021-05-13 22:09:22 -0300

    adding middleware to check mime type

---
+++ src/middlewares/checkMimeType.ts
@@ -0,0 +1,22 @@
+import { UserInputError } from 'apollo-server-express';
+import FileType from 'file-type';
+import { ReadStream } from 'node:fs';
+
+const checkMimeType = async (stream: ReadStream) => {
+  const validTypes = ['png', 'jpg', 'jpeg', 'webp', 'mp3', 'wav'];
+  const mimeType = await FileType.fromStream(stream);
+
+  if (!mimeType) {
+    throw new Error();
+  }
+
+  const isValidType = validTypes.includes(mimeType.ext);
+
+  if (!isValidType) {
+    throw new UserInputError('Formato não suportado');
+  }
+
+  return mimeType.mime;
+};
+
+export default checkMimeType;
