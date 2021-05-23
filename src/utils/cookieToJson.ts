eef64dd258d2a1ab7449bfbf1984d6f0d21a1005
yuri014 <yuriflores2012@gmail.com>
2021-05-23 20:39:45 -0300

    convert cookie to json

---
+++ src/utils/cookieToJson.ts
@@ -0,0 +1,11 @@
+const cookieToJson = (cookie: string): { [key: string]: string } =>
+  cookie.split(';').reduce((res, c) => {
+    const [key, val] = c.trim().split('=').map(decodeURIComponent);
+    try {
+      return Object.assign(res, { [key]: JSON.parse(val) });
+    } catch (e) {
+      return Object.assign(res, { [key]: val });
+    }
+  }, {});
+
+export default cookieToJson;
