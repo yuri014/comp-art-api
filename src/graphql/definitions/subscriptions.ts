d2c8b2c87b0d73a8f90c93f8fe42d9ebb6fab4e9
yuri014 <yuriflores2012@gmail.com>
2021-05-24 20:01:04 -0300

    adding avatar and from on notification type

--- src/graphql/definitions/subscriptions.ts
+++ src/graphql/definitions/subscriptions.ts
@@ -3,11 +3,12 @@ import { gql } from 'apollo-server-express';
 const subscriptions = gql`
   type Notification {
     _id: String
-    title: String
+    from: String
     body: String
     createdAt: String
     link: String
     read: Boolean
+    avatar: String
   }
 
   type Subscription {
