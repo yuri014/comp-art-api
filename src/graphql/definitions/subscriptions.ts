eb755669217c318249629f033a1f66c31a1d9a10
yuri014 <yuriflores2012@gmail.com>
2021-05-24 20:18:05 -0300

    adding notifications types from subscription file

--- src/graphql/definitions/subscriptions.ts
+++ src/graphql/definitions/subscriptions.ts
@@ -1,16 +1,6 @@
 import { gql } from 'apollo-server-express';
 
 const subscriptions = gql`
-  type Notification {
-    _id: String
-    from: String
-    body: String
-    createdAt: String
-    link: String
-    read: Boolean
-    avatar: String
-  }
-
   type Subscription {
     notification: Notification!
   }
