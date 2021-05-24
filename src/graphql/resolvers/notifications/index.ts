f2b5e309c5a6a387cc90e24bf2e326b6ac27795a
yuri014 <yuriflores2012@gmail.com>
2021-05-24 20:18:45 -0300

    get notification with pagination

--- src/graphql/resolvers/notifications/index.ts
+++ src/graphql/resolvers/notifications/index.ts
@@ -1,7 +1,22 @@
 import { IResolvers, withFilter } from 'apollo-server-express';
+import Notification from '../../../entities/Notification';
 import checkAuth from '../../../middlewares/checkAuth';
 
 const notificationsResolvers: IResolvers = {
+  Query: {
+    async getNotifications(_, { offset }: { offset: number }, context) {
+      const user = checkAuth(context);
+      const notifications = await Notification.findOne({ user: user.id })
+        .where('notifications')
+        .slice([offset > 0 ? Math.round(offset / 2) : offset, offset + 4]);
+
+      if (!notifications) {
+        return [];
+      }
+
+      return notifications?.notifications;
+    },
+  },
   Subscription: {
     notification: {
       subscribe: withFilter(
