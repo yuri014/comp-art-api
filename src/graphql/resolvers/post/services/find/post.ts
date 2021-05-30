ff2ed5b0508da139e5c822fb6ce94573c53c5ae4
yuri014 <yuriflores2012@gmail.com>
2021-05-29 21:52:10 -0300

    reusing handlePostView in single post query

--- src/graphql/resolvers/post/services/find/post.ts
+++ src/graphql/resolvers/post/services/find/post.ts
@@ -1,5 +1,6 @@
 import getUser from '../../../../../auth/getUser';
 import Post from '../../../../../entities/Post';
+import { handlePostView } from '../utils/postUtils';
 
 const getPostService = async (id: string, token: string) => {
   const user = getUser(token);
@@ -12,8 +13,9 @@ const getPostService = async (id: string, token: string) => {
   if (post) {
     // @ts-ignore
     const isLiked = post.likes.find(like => like.profile.owner === user.username);
+    const { isSaved, imageHeight } = await handlePostView(post, user.id);
 
-    return { ...post._doc, isLiked: !!isLiked };
+    return { ...post._doc, isLiked: !!isLiked, isSaved, imageHeight };
   }
 
   return {};
