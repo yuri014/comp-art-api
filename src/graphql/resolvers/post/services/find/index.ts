cceedc5f47774ea995550a8448fb5322e697d29b
yuri014 <yuriflores2012@gmail.com>
2021-05-29 22:27:40 -0300

    adding find post object

---
+++ src/graphql/resolvers/post/services/find/index.ts
@@ -0,0 +1,15 @@
+import getExplorePostsService from './explorePosts';
+import getTimelinePosts from './timeline';
+import getPostLikes from './likes';
+import getProfilePostsService from './profilePosts';
+import getPostService from './post';
+
+const FindPosts = {
+  getExplorePostsService,
+  getTimelinePosts,
+  getPostLikes,
+  getProfilePostsService,
+  getPostService,
+};
+
+export default FindPosts;
