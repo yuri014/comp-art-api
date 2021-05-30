6c10daf2f8ab112f44b737685a4bb7ad1e5f52b4
yuri014 <yuriflores2012@gmail.com>
2021-05-29 22:38:40 -0300

    removing getPostByTag because searchPost can do it

--- src/graphql/definitions/query.ts
+++ src/graphql/definitions/query.ts
@@ -27,7 +27,6 @@ const queries = gql`
     getProfilePosts(offset: Int!, username: String!): [Timeline]
     getExplorePosts(offset: Int!): [Post]
     searchPost(query: String!, offset: Int!): [Post]
-    getPostByTag(tag: String!, offset: Int!): [Timeline]
 
     #post interactions
     getComments(postID: ID!, offset: Int!): [Comments]
