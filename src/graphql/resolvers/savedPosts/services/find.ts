f4f2feecb701215fb9f9d7c337dcd01ae027c58a
yuri014 <yuriflores2012@gmail.com>
2021-06-05 17:33:14 -0300

    adding view data

--- src/graphql/resolvers/savedPosts/services/find.ts
+++ src/graphql/resolvers/savedPosts/services/find.ts
@@ -3,6 +3,7 @@ import { UserInputError } from 'apollo-server-express';
 import SavedPost from '../../../../entities/SavedPost';
 import { IPost } from '../../../../interfaces/Post';
 import { IToken } from '../../../../interfaces/Token';
+import { getUserLikes, handlePostView, ILikes } from '../../post/services/utils/postUtils';
 
 const getSavedPostsService = async (user: IToken, offset: number) => {
   const savedPosts = await SavedPost.findOne({ user: user.id })
@@ -33,11 +34,15 @@ const getSavedPostsService = async (user: IToken, offset: number) => {
     throw new UserInputError('Não há posts salvos');
   }
 
-  const posts = savedPosts.posts.map(({ post }) => {
+  const likes = getUserLikes((savedPosts.posts as unknown) as IPost[], user.username);
+
+  const posts = savedPosts.posts.map(async ({ post }, index) => {
     const newPosts = post as IPost;
     const postDoc = newPosts._doc;
+    const { imageHeight, isSaved, getIsLiked } = await handlePostView(newPosts, user.id);
+    const isLiked = getIsLiked(likes as ILikes, index);
 
-    return { ...postDoc, likes: postDoc?.likes.slice(0, 1) };
+    return { ...postDoc, likes: postDoc?.likes.slice(0, 1), isLiked, isSaved, imageHeight };
   });
 
   return posts;
