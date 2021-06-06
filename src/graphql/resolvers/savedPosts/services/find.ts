247b381a9d1c0d8226828b626e87364b05c04cd1
yuri014 <yuriflores2012@gmail.com>
2021-06-06 02:23:37 -0300

    get saved ṕost logic

--- src/graphql/resolvers/savedPosts/services/find.ts
+++ src/graphql/resolvers/savedPosts/services/find.ts
@@ -2,15 +2,16 @@ import { UserInputError } from 'apollo-server-express';
 
 import SavedPost from '../../../../entities/SavedPost';
 import { IPost } from '../../../../interfaces/Post';
+import { IShare } from '../../../../interfaces/Share';
 import { IToken } from '../../../../interfaces/Token';
-import { getUserLikes, handlePostView, ILikes } from '../../post/services/utils/postUtils';
+import getImageHeight from '../../post/services/utils/getImageHeight';
+import { getIsLiked } from '../../post/services/utils/postUtils';
+import findProfile from '../../profiles/services/utils/findProfileUtil';
 
 const getSavedPostsService = async (user: IToken, offset: number) => {
   const savedPosts = await SavedPost.findOne({ user: user.id })
     .skip(offset)
     .limit(10)
-    .populate('posts.post')
-    .slice('posts.post.1.likes', [0, 1])
     .populate({
       path: 'posts.post',
       populate: {
@@ -26,24 +27,58 @@ const getSavedPostsService = async (user: IToken, offset: number) => {
     .populate({
       path: 'posts.post',
       populate: {
+        path: 'post',
+        populate: {
+          path: 'artist',
+        },
+      },
+    })
+    .populate({
+      path: 'posts.post',
+      populate: {
         path: 'likes.profile',
       },
-    });
+    })
+    .lean();
+
+  const profile = await findProfile(user);
 
   if (!savedPosts) {
     throw new UserInputError('Não há posts salvos');
   }
 
-  const likes = getUserLikes((savedPosts.posts as unknown) as IPost[], user.username);
+  const posts = await Promise.all(
+    savedPosts.posts.map(async ({ post }) => {
+      const newPosts = post as IPost;
 
-  const posts = savedPosts.posts.map(async ({ post }, index) => {
-    const newPosts = post as IPost;
-    const postDoc = newPosts._doc;
-    const { imageHeight, isSaved, getIsLiked } = await handlePostView(newPosts, user.id);
-    const isLiked = getIsLiked(likes as ILikes, index);
+      const handleImageHeight = () => {
+        if (newPosts.artist) {
+          return getImageHeight(newPosts);
+        }
 
-    return { ...postDoc, likes: postDoc?.likes.slice(0, 1), isLiked, isSaved, imageHeight };
-  });
+        const share = post as IShare;
+        const sharedPost = share.post as IPost;
+
+        return getImageHeight(sharedPost);
+      };
+
+      const imageHeight = handleImageHeight();
+
+      const isLiked = await getIsLiked({
+        isShare: !newPosts.artist,
+        postID: newPosts._id,
+        profileID: profile._doc?._id,
+      });
+
+      return {
+        ...newPosts,
+        likes: newPosts?.likes.slice(0, 1),
+        isLiked,
+        isSaved: true,
+        imageHeight,
+      };
+    }),
+  );
 
   return posts;
 };
