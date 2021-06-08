7be7eed9e4fd82190dbeecc3899a5a1b15973901
yuri014 <yuriflores2012@gmail.com>
2021-06-07 21:58:31 -0300

    fix comment validation

--- src/validators/commentSchema.ts
+++ src/validators/commentSchema.ts
@@ -3,9 +3,11 @@ import Joi from 'joi';
 const isValidText = Joi.string();
 
 const commentValidationSchema = Joi.object({
-  comment: isValidText.allow('').max(255).messages({
-    'string.base': 'Descrição deve ser um texto',
-    'string.min': 'Descrição limite de 255 caracteres',
+  // eslint-disable-next-line newline-per-chained-call
+  comment: isValidText.required().min(1).max(255).messages({
+    'string.base': 'Comentário deve ser um texto',
+    'string.min': 'Comentário precisa de pelo menos um caracter',
+    'string.max': 'Comentário limite de 255 caracteres',
   }),
 });
 
