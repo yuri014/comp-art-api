d1a1e75ad766fda944605f64104cce53a1f0e2a5
yuri014 <yuriflores2012@gmail.com>
2021-06-13 23:43:42 -0300

    removing function to put in utils folder

--- src/graphql/resolvers/profiles/services/find/follow.ts
+++ src/graphql/resolvers/profiles/services/find/follow.ts
@@ -2,43 +2,11 @@ import getUser from '../../../../../auth/getUser';
 import Follower from '../../../../../entities/Follower';
 import Following from '../../../../../entities/Following';
 import { IFollower, IFollowing } from '../../../../../interfaces/Follow';
-import { IProfileEntity } from '../../../../../interfaces/Models';
-import { IArtistProfile, IUserProfile } from '../../../../../interfaces/Profile';
 import { IToken } from '../../../../../interfaces/Token';
 import { isAlreadyFollowing } from '../../../../../middlewares/isAlreadyFollow';
+import followersWithAuth from '../utils/findFollowersWithAtuht';
 import findFollows, { IOffset } from '../utils/findFollows';
 import shuffleArray from '../utils/shuffleProfilesArray';
-
-const isFollowingLoggedUser = async (profile: IProfileEntity, username: string) => {
-  const isFollowing = await Follower.findOne({
-    username,
-    // @ts-ignore
-    $or: [{ artistFollowers: profile._id }, { userFollowers: profile._id }],
-  }).lean();
-
-  if (isFollowing) {
-    return { ...profile?._doc, followsYou: true };
-  }
-
-  return { ...profile?._doc, followsYou: false };
-};
-
-const followersWithAuth = async (
-  user: IToken | string,
-  artists: IArtistProfile[],
-  users: IUserProfile[],
-) => {
-  const authUser = user as IToken;
-
-  const artistsWithAuth = await Promise.all(
-    artists.map(artist => isFollowingLoggedUser(artist, authUser.username)),
-  );
-  const usersWithAuth = await Promise.all(
-    users.map(_user => isFollowingLoggedUser(_user, authUser.username)),
-  );
-
-  return shuffleArray(artistsWithAuth, usersWithAuth);
-};
 
 export const getFollowingService = async (params: IOffset, token: string) => {
   const followsResult = await findFollows(Following, params, ['artistFollowing', 'userFollowing']);
