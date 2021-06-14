91e42b4a7c0caaf6b7e0d197784de10092ef9f36
yuri014 <yuriflores2012@gmail.com>
2021-06-13 23:43:54 -0300

    adding follows you and is following logic

---
+++ src/graphql/resolvers/profiles/services/utils/findFollowersWithAtuht.ts
@@ -0,0 +1,39 @@
+import Follower from '../../../../../entities/Follower';
+import Following from '../../../../../entities/Following';
+import { IProfileEntity } from '../../../../../interfaces/Models';
+import { IArtistProfile, IUserProfile } from '../../../../../interfaces/Profile';
+import { IToken } from '../../../../../interfaces/Token';
+import shuffleArray from './shuffleProfilesArray';
+
+const isFollowingLoggedUser = async (profile: IProfileEntity, username: string) => {
+  const followsYou = await Follower.findOne({
+    username,
+    $or: [{ artistFollowers: profile._id }, { userFollowers: profile._id }],
+  }).lean();
+
+  const isFollowing = await Following.findOne({
+    username,
+    $or: [{ artistFollowing: profile._id }, { userFollowing: profile._id }],
+  }).lean();
+
+  return { ...profile?._doc, followsYou: !!followsYou, isFollowing: !!isFollowing };
+};
+
+const followersWithAuth = async (
+  user: IToken | string,
+  artists: IArtistProfile[],
+  users: IUserProfile[],
+) => {
+  const authUser = user as IToken;
+
+  const artistsWithAuth = await Promise.all(
+    artists.map(artist => isFollowingLoggedUser(artist, authUser.username)),
+  );
+  const usersWithAuth = await Promise.all(
+    users.map(_user => isFollowingLoggedUser(_user, authUser.username)),
+  );
+
+  return shuffleArray(artistsWithAuth, usersWithAuth);
+};
+
+export default followersWithAuth;
