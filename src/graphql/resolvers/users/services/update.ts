4f0fc36b0a1ce9a6d04a75e77dfdd70e05071375
yuri014 <yuriflores2012@gmail.com>
2021-06-19 21:47:51 -0300

    fixing error handlers and generating token on confirm email

--- src/graphql/resolvers/users/services/update.ts
+++ src/graphql/resolvers/users/services/update.ts
@@ -5,48 +5,53 @@ import { UserInputError } from 'apollo-server-express';
 import User from '../../../../entities/User';
 import { ID } from '../../../../interfaces/General';
 import ConfirmationCode from '../../../../entities/ConfirmationCode';
+import generateToken from '../../../../generators/generateToken';
 
 export const confirmUser = async (code: string, email: string) => {
-  try {
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (!user) {
-      throw new UserInputError('Não há usuário com esse email!');
-    }
-
-    const confirmationCode = await ConfirmationCode.findOne({
-      user: user?._id,
-      code,
-    });
-
-    if (!confirmationCode) {
-      throw new UserInputError('Oops... Código incorreto, tente novamente!');
-    }
-
-    const userById = await User.findByIdAndUpdate(
-      user.id,
-      { confirmed: true },
-      { useFindAndModify: false },
-    );
-    if (!userById) {
-      throw new Error();
-    }
-    return userById;
-  } catch (error) {
-    throw new Error(error);
+  if (!user) {
+    throw new UserInputError('Não há usuário com esse email!');
   }
+
+  const confirmationCode = await ConfirmationCode.findOne({
+    user: user?._id,
+    code,
+  });
+
+  if (!confirmationCode) {
+    throw new UserInputError('Oops... Código incorreto, tente novamente!');
+  }
+
+  const userById = await User.findByIdAndUpdate(
+    user.id,
+    { confirmed: true },
+    { useFindAndModify: false },
+  );
+  if (!userById) {
+    throw new Error();
+  }
+
+  const token = generateToken(user, '7d');
+
+  return {
+    id: user._id,
+    token,
+    username: user.username,
+    isArtist: user.isArtist,
+  };
 };
 
 export const updatePassword = async (token: string, newPassword: string) => {
-  try {
-    const { id } = jwt.verify(token, process.env.SECRET as string) as ID;
-    const user = await User.findById(id);
-    const encryptedPassword = await bcrypt.hash(newPassword, 12);
-    if (user) {
-      await User.updateOne({ password: encryptedPassword });
-    }
-    return token;
-  } catch (error) {
-    throw new Error(error);
+  const { id } = jwt.verify(token, process.env.SECRET as string) as ID;
+  const user = await User.findById(id);
+
+  if (user) {
+    throw new UserInputError('Não existe usuário');
   }
+
+  const encryptedPassword = await bcrypt.hash(newPassword, 12);
+  await User.updateOne({ password: encryptedPassword });
+
+  return token;
 };
