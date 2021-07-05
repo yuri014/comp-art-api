1464f1c682259acb56a9678e054b39c1b30e379f
yuri014 <yuriflores2012@gmail.com>
2021-07-04 21:38:37 -0300

    decrement following and follower

---
+++ src/graphql/resolvers/users/services/utils/decrementFollow.ts
@@ -0,0 +1,28 @@
+import { IProfileModel } from '../../../../../interfaces/Models';
+
+interface DecrementFollow {
+  follower: string[] | undefined;
+  Entity: IProfileModel;
+  field: string;
+}
+
+const decrementFollow = async ({ follower, Entity, field }: DecrementFollow) => {
+  if (follower) {
+    await Promise.all(
+      follower.map(async (id: string) =>
+        // @ts-ignore
+        Entity.findByIdAndUpdate(
+          id,
+          {
+            $inc: {
+              [field]: -1,
+            },
+          },
+          { useFindAndModify: false },
+        ),
+      ),
+    );
+  }
+};
+
+export default decrementFollow;
