c126f5206bdab3f890b1b7c9b29626b2538e93a1
yuri014 <yuriflores2012@gmail.com>
2021-07-04 21:38:50 -0300

    adding profile model type

--- src/interfaces/Models.ts
+++ src/interfaces/Models.ts
@@ -7,3 +7,5 @@ import { IShare } from './Share';
 export type PostEntity = Model<IPost> | Model<IShare>;
 
 export type IProfileEntity = IArtistProfile | IUserProfile;
+
+export type IProfileModel = Model<IArtistProfile> | Model<IUserProfile>;
