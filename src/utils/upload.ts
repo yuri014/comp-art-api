5ecc621c831ab3bec5f86de1cb62bb3a947194a5
yuri014 <yuriflores2012@gmail.com>
2021-07-06 19:04:33 -0300

    upload in dev mode

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -1,48 +1,33 @@
-import path from 'path';
-import fs, { ReadStream } from 'fs-extra';
 import { UserInputError } from 'apollo-server-express';
 
-export const uploadImage = async (createReadStream?: () => ReadStream, filename?: string) => {
+import writeFileDev from './writeFileDev';
+import { ICreateReadStream } from '../interfaces/General';
+
+const __DEV__ = process.env.NODE_ENV === 'development';
+
+export const uploadImage = async (createReadStream?: ICreateReadStream, filename?: string) => {
   if (filename && !filename.match(/\.(png|jpg|jpeg|webp)$/)) {
     throw new UserInputError(
       'Upload precisa ser em um formato de imagem suportado. Formatos de imagens suportados: png, webp, jpg e jpeg.',
     );
   }
 
-  try {
-    if (createReadStream && filename) {
-      const stream = createReadStream();
-      const originalName = `${Date.now()}-${filename}`;
-      const pathName = path.join(__dirname, '..', '..', `/public/uploads/images/${originalName}`);
-
-      stream.pipe(fs.createWriteStream(pathName));
-      return `/uploads/images/${originalName}`;
-    }
-  } catch (error) {
-    throw new UserInputError('Limite máximo de upload: 8MB.');
+  if (__DEV__) {
+    return writeFileDev({ createReadStream, filename, folderPath: '/uploads/images/' });
   }
 
   return '';
 };
 
-export const uploadAudio = async (createReadStream?: () => ReadStream, filename?: string) => {
+export const uploadAudio = async (createReadStream?: ICreateReadStream, filename?: string) => {
   if (filename && !filename.match(/\.(mp3|wav)$/)) {
     throw new UserInputError(
       'Upload precisa ser em um formato de imagem suportado. Formatos de imagens suportados: mp3 e wav.',
     );
   }
 
-  try {
-    if (createReadStream && filename) {
-      const stream = createReadStream();
-      const originalName = `${Date.now()}-${filename}`;
-      const pathName = path.join(__dirname, '..', '..', `/public/uploads/audio/${originalName}`);
-
-      stream.pipe(fs.createWriteStream(pathName));
-      return `/uploads/audio/${originalName}`;
-    }
-  } catch (error) {
-    throw new UserInputError('Limite máximo de upload: 8MB.');
+  if (__DEV__) {
+    return writeFileDev({ createReadStream, filename, folderPath: '/uploads/audio/' });
   }
   return '';
 };
