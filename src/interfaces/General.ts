500805812ae628ce28bf8b0bc464a37851d4c5a9
yuri014 <yuriflores2012@gmail.com>
2021-07-06 19:04:09 -0300

    adding create read stream type

--- src/interfaces/General.ts
+++ src/interfaces/General.ts
@@ -1,3 +1,7 @@
+import { ReadStream } from 'fs-extra';
+
 export type IOnModel = 'ArtistProfile' | 'UserProfile';
 
 export type ID = { id: string };
+
+export type ICreateReadStream = () => ReadStream;
