375f1b9452489e71bc074cb1eb868c0e5bc92719
yuri014 <yuriflores2012@gmail.com>
2021-07-06 21:50:37 -0300

    fixes remove file for production

--- src/utils/removeFile.ts
+++ src/utils/removeFile.ts
@@ -1,9 +1,22 @@
 import path from 'path';
 import fs from 'fs-extra';
+import S3, { PutObjectRequest } from 'aws-sdk/clients/s3';
 
 const removeFile = async (pathname: string) => {
-  const pathName = path.join(__dirname, '..', '..', `/public${pathname}`);
-  return fs.remove(pathName);
+  const url = new URL(pathname);
+
+  if (globalThis.__DEV__) {
+    const pathName = path.join(__dirname, '..', '..', `/public${url.pathname}`);
+    fs.remove(pathName);
+  } else {
+    const params: PutObjectRequest = {
+      Bucket: process.env.AWS_S3_BUCKET as string,
+      Key: url.pathname,
+    };
+    const s3 = new S3({ apiVersion: '2006-03-01', region: process.env.AWS_REGION });
+
+    s3.deleteObject(params).promise();
+  }
 };
 
 export default removeFile;
