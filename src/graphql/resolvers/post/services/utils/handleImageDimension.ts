83ba83a1ae510f7245ea878480e8b47e7f3ffac4
yuri014 <yuriflores2012@gmail.com>
2021-07-06 21:42:13 -0300

    fix get image size for http

--- src/graphql/resolvers/post/services/utils/handleImageDimension.ts
+++ src/graphql/resolvers/post/services/utils/handleImageDimension.ts
@@ -1,15 +1,33 @@
 import { UserInputError } from 'apollo-server-express';
-import path from 'path';
 import getImageSize from 'image-size';
+import http from 'https';
+import { ISizeCalculationResult } from 'image-size/dist/types/interface';
 
-const handleImageDimension = (uploadPath: string) => {
-  const realPath = path.join(__dirname, '..', '..', '..', '..', '..', '..', `/public${uploadPath}`);
-  const dimensions = getImageSize(realPath);
+const handleImageDimension = async (uploadPath: string) => {
+  const options = new URL(uploadPath);
 
-  const imageHeight = dimensions.height;
+  const getDimensions = () => {
+    const dimensions = new Promise((resolve: (size: ISizeCalculationResult) => void) => {
+      http.get(options, response => {
+        const chunks: Uint8Array[] = [];
+        response
+          .on('data', chunk => {
+            chunks.push(chunk);
+          })
+          .on('end', () => {
+            const buffer = Buffer.concat(chunks);
+            resolve(getImageSize(buffer));
+          });
+      });
+    });
+
+    return dimensions;
+  };
+
+  const imageHeight = await getDimensions().then(result => result.height);
 
   if (!imageHeight) {
-    throw new UserInputError('Arquivo precisa ser uma image.');
+    throw new UserInputError('Arquivo precisa ser uma imagem.');
   }
 
   if (imageHeight > 600) {
