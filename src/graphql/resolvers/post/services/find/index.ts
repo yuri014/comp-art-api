f0caf3acd655e8eb11256351695e9b5774e05e6e
yuri014 <yuriflores2012@gmail.com>
2021-07-16 20:17:44 -0300

    adding profile post and share services

--- src/graphql/resolvers/post/services/find/index.ts
+++ src/graphql/resolvers/post/services/find/index.ts
@@ -1,17 +1,19 @@
 import getExplorePostsService from './explorePosts';
 import getTimelinePosts from './timeline';
 import getPostLikes from './likes';
-import getProfilePostsService from './profilePosts';
+import getProfilePostsAndSharesService from './profilePostsAndShares';
 import getPostService from './post';
+import getArtistPosts from './artistsPosts';
 import searchPostService from './search';
 
 const FindPosts = {
   getExplorePostsService,
   getTimelinePosts,
   getPostLikes,
-  getProfilePostsService,
   getPostService,
+  getArtistPosts,
   searchPostService,
+  getProfilePostsAndSharesService,
 };
 
 export default FindPosts;
